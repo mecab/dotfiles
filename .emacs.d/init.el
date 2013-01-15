947e4c11d03bab8076c3d82a7da7c76a0d636771
mecab <mecab@misosi.ru>
2013-01-16 01:36:11 +0900

    init

---
+++ .emacs.d/init.el
@@ -0,0 +1,42 @@
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(add-to-list 'load-path "~/.emacs.d/auto-install/")
+
+(require 'auto-install)
+(setq auto-install-directory "~/.emacs.d/auto-install/")
+(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup)
+
+(require 'anything-startup)
+
+
+(defun my-move-beginning-of-line ()
+  (interactive)
+  (if (bolp)
+      (back-to-indentation)    
+      (beginning-of-line)))
+(global-set-key "\C-a" 'my-move-beginning-of-line)
+
+(defun my-end-of-line ()
+  (interactive)
+  (end-of-line)
+  (if (eq last-command this-command)
+      (delete-horizontal-space)))
+(global-set-key "\C-e" 'my-end-of-line)
+
+;; shell-pop
+;; C-tでshellをポップアップ
+(require 'shell-pop)
+(shell-pop-set-internal-mode "ansi-term") ;; ansi-termを使うよ
+(shell-pop-set-internal-mode-shell "/bin/zsh") ;; zshを使うよ
+(defvar ansi-term-after-hook nil)
+(add-hook 'ansi-term-after-hook
+          '(lambda ()
+             (define-key term-raw-map "\C-t" 'shell-pop)))
+(defadvice ansi-term (after ansi-term-after-advice (org))
+  "run hook as after advice"
+  (run-hooks 'ansi-term-after-hook))
+(ad-activate 'ansi-term)
+(global-set-key "\C-t" 'shell-pop)
+
+(global-set-key "\C-h" 'delete-backward-char)
+
