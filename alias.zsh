28a5141c62da983cf1bae79786c7279866366865
masafumi.muta <masafumi.muta@rakuten.com>
2023-06-01 09:45:20 +0100

    Update alias

--- alias.zsh
+++ alias.zsh
@@ -54,8 +54,8 @@ if type kubectl >/dev/null 2>&1; then
     alias kgan='kubectl get all --all-namespaces'
     alias kga='kubectl get all'
     alias kgi='kubectl get pods -o jsonpath="{..containers..image}" | tr -s "[[:space:]]" "\n" | sort | uniq'
-    alias ktx='kubectl config use-context $(kubectl config get-contexts -o name | fzf --height=50%)'
-    alias kns='kubectl config set-context --current --namespace=$(kubectl get namespaces --no-headers -o custom-columns=':metadata.name' | fzf --height=50%)'
+    alias ktx='(){ if [[ -n "$1" ]]; then kubectl config use-context "$1"; else kubectl config use-context $(kubectl config get-contexts -o name | fzf --height=50%); fi }'
+    alias kns='(){ if [[ -n "$1" ]]; then kubectl config set-context --current --namespace="$1"; else kubectl config set-context --current --namespace=$(kubectl get namespaces --no-headers -o custom-columns=':metadata.name' | fzf --height=50%); fi }'
     alias kezf='(){ kubectl exec -it $(kubectl get pods -oname | sed -E "s/pod\///g" | fzf) $@ }'
 
     alias -g K='$(kubectl get pods -oname | sed -E "s/pod\///g" | fzf --height=50% --header=pods)'
@@ -65,3 +65,4 @@ fi
 alias randomstr='(){ cat /dev/urandom | LC_CTYPE=C tr -dc "0-9a-zA-Z" | fold -w $1 | head -n 1 }'
 alias colorize_rbl="colorecho -w -p '/- debug: /,gray' -p '/- info: /,cyan' -p '/- error: /,red' -p '/\[hostName=.*?\]/,blue' -p '/\[serviceName=.*?\]/,green' -p '/\[correlationId=.*?\]/,yellow' -p '/\[clientId=.*?\]/,magenta'"
 alias branch_from_issue="git checkout -b \$(jira issue list -q'project IS NOT EMPTY' -s Open -a `jira me` --plain --no-headers --columns key,summary | fzf --height=50% | perl -pE 's/[ \t]/-/g;' -pE 's/[^a-zA-Z0-9 _-]//g;' -pE 's/[-]+/-/g;' -pE 's/^(.+?)-([0-9]+)-(.+)/\$1-\$2-\L\$3/g');"
+alias hl='(){ egrep --color "$|.*$1.*" }'
